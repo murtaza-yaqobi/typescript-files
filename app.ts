a70d07f97f21bafecd1dea3c67d3f004054a4db1
murtaza-yaqobi <murtazayaqobi7@gmail.com>
2025-07-16 15:45:50 +0430

    Typescript

--- app.ts
+++ app.ts
@@ -167,7 +167,7 @@ function jaghori(
   if (typeof hutqol === "string" && typeof angori === "string") {
     return hutqol + angori;
   }
-  throw new Error("Function overloading Error💀");
+  throw new Error("Function overloading Error!");
 }
 console.log(jaghori("Hutqoli ", "Boy"));
 console.log(jaghori(100, 45));
