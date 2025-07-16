5800e084270a86841fcbecc632b119f2a0d55c81
murtaza-yaqobi <murtazayaqobi7@gmail.com>
2025-07-16 15:47:36 +0430

    Added

--- app.ts
+++ app.ts
@@ -170,4 +170,5 @@ function jaghori(
   throw new Error("Function overloading Error!☠️💀");
 }
 console.log(jaghori("Hutqoli ", "Boy"));
+console.log(jaghori("Murtaza", "Yaqobi"));
 console.log(jaghori(100, 45));
