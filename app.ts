a585665b7bbe3414e4c825be15309a6096e50f9f
murtaza-yaqobi <murtazayaqobi7@gmail.com>
2025-07-22 05:57:03 +0430

    |

--- app.ts
+++ app.ts
@@ -182,7 +182,5 @@ console.log(jaghori("Murtaza", "Yaqobi"));
 console.log(jaghori(100, 300));
 console.log(jaghori(100, 300));
 console.log(jaghori(100, 300));
-console.log(jaghori(100, 300));
-console.log(jaghori(100, 300));
 
 console.log(jaghori(100, 45));
