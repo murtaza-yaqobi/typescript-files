6a2f37eca048a47697c7a9f72db280ecfbbdaff1
murtaza-yaqobi <murtazayaqobi7@gmail.com>
2025-07-27 21:16:40 +0430

    New typescript

--- app.ts
+++ app.ts
@@ -181,5 +181,3 @@ function jaghori(
 console.log(jaghori("Murtaza", "Yaqobi"));
 console.log(jaghori(100, 300));
 console.log(jaghori(100, 45));
-console.log(jaghori(100, 45));
-console.log(jaghori(100, 45));
